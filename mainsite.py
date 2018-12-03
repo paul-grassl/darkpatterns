2f829cf5a311eba38ac4d4f1623aaf40b32fc3d6
Paul <paulklee@mailbox.org>
2018-12-03 16:59:41 +0100

    preparing layout.html for bootstrap

--- mainsite.py
+++ mainsite.py
@@ -9,6 +9,21 @@ def index():
     return render_template('index.html')
 
 
+@app.route("/welcome")
+def welcome():
+    return render_template('welcome.html', title='Welcome')
+
+
+@app.route("/newswebsite1")
+def newswebsite1():
+    return render_template('newswebsite1_viral.html')
+
+
+@app.route("/questionnaire_control")
+def questionnaire_control():
+    return render_template('questionnaire_control.html', title='Questionnaire 1')
+
+
 # This will run the application in debug mode by default, we may want to change
 # that before going online
 if __name__ == '__main__':
