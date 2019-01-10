23fc7eebe9f8c7644edf6353d6ba9e9b63fe68d1
Paul <paulklee@mailbox.org>
2019-01-10 17:17:06 +0100

    DecimalRangeField click check/validation works (JavaScript)

---
+++ static/script.js
@@ -0,0 +1,50 @@
+$(document).ready(function () {
+  var slider1Clicked = false
+  var slider2Clicked = false
+  var slider3Clicked = false
+  var slider4Clicked = false
+  var slider5Clicked = false
+  var slider6Clicked = false
+
+  // listen for click on element with id 'slider'
+  // run the passed function when element is clicked
+  $('#perceivedControlQ1').click(function () {
+    slider1Clicked = true
+    alert('Slider 1 clicked!')
+  })
+
+  $("#perceivedControlQ2").click(function() {
+    slider2Clicked = true;
+  });
+
+  $("#perceivedControlQ3").click(function() {
+    slider3Clicked = true;
+  });
+
+  $("#perceivedControlQ4").click(function() {
+    slider4Clicked = true;
+  });
+
+  $("#perceivedControlQ5").click(function() {
+    slider5Clicked = true;
+  });
+
+  $("#deliberation").click(function() {
+    slider6Clicked = true;
+  });
+
+  // listen for click on element with id 'submit-btn'
+  $('#submit').click(function(evt) {
+    if (slider1Clicked && slider2Clicked &&
+        slider3Clicked && slider4Clicked &&
+        slider5Clicked && slider6Clicked) {
+      alert('Submitting the form!');
+    }
+    else {
+      alert('Wait! You must click both sliders before you can submit the form');
+      // prevent the default action fort this even
+      // a submit button in a form has a default action to submit the form
+      evt.preventDefault();
+    }
+  })
+});
--- static/scriptClickCheck.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function clickCheck(sliderNumber,id){
-    sliderNumber = false;
-  $("#id").click(function(){
-    sliderNumber = true;
-  });
-});
