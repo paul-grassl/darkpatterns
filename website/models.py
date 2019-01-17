7f9eb1f6464f03d69a3daecfeec3bad8a7697dd0
Paul <paulklee@mailbox.org>
2019-01-17 15:28:16 +0100

    Testing data saving of demographic info

--- website/models.py
+++ website/models.py
@@ -3,17 +3,23 @@ from website import db
 
 # models for database structure (each one represents a questionnaire)
 class demographicData(db.Model):
+    __tablename__ = 'demographic_data'
     id = db.Column(db.Integer, primary_key=True)
     gender = db.Column(db.String(1), nullable=False)
     age = db.Column(db.Integer, nullable=False)
     nationality = db.Column(db.String(50), nullable=False)
+    # create relationships to the other models to link them to the demographicData
+    questionnaire1 = db.relationship('controlAndDeliberationData', backref='participant', lazy=True)
+    questionnaire2 = db.relationship('privacyConcernsData', backref='participant', lazy=True)
 
     def __repr__(self):
-        return f"demographicInfo('{self.id}', '{self.gender}', '{self.age}', '{self.nationality}')"
+        return f"demographicData('{self.id}', '{self.gender}', '{self.age}', '{self.nationality}')"
+
 
 class controlAndDeliberationData(db.Model):
+    __tablename__ = 'control_and_deliberation_data'
     id = db.Column(db.Integer, primary_key=True)
-    participantId = db.Column(db.Integer, db.ForeignKey('demographicData.id'), nullable=False)
+    participantId = db.Column(db.Integer, db.ForeignKey('demographic_data.id'), nullable=False)
     perceivedControlQ1 = db.Column(db.Integer, nullable=False)
     perceivedControlQ2 = db.Column(db.Integer, nullable=False)
     perceivedControlQ3 = db.Column(db.Integer, nullable=False)
@@ -25,9 +31,11 @@ class controlAndDeliberationData(db.Model):
     def __repr__(self):
         return f"controlAndDeliberationData('{self.participantId}', '{self.perceivedControlQ1}', '{self.perceivedControlQ2}', '{self.perceivedControlQ3}', '{self.perceivedControlQ4}', '{self.perceivedControlQ5}', '{self.manipulationCheck}', '{self.deliberation}')"
 
+
 class privacyConcernsData(db.Model):
+    __tablename__ = 'privacy_concerns_data'
     id = db.Column(db.Integer, primary_key=True)
-    participantId = db.Column(db.Integer, db.ForeignKey('demographicData.id'), nullable=False)
+    participantId = db.Column(db.Integer, db.ForeignKey('demographic_data.id'), nullable=False)
     privacyConcernsQ1 = db.Column(db.String(1), nullable=False)
     privacyConcernsQ2 = db.Column(db.String(1), nullable=False)
     privacyConcernsQ3 = db.Column(db.String(1), nullable=False)
--- website/routes.py
+++ website/routes.py
@@ -3,12 +3,14 @@ from website import app, db
 from website.models import demographicData, controlAndDeliberationData, privacyConcernsData
 from website.questionnaires import demographicsForm, websiteDesignForm, controlAndDeliberationForm, privacyConcernsForm, welcomeForm
 import random
-from website import stimuliList, security
+from website import stimuliList
+
 
 # make a list of all websites and then shuffle randomly for each participant
 websiteList = list(stimuliList.stimuli.keys())
 
 
+# route to welcome page with permission statement and consent form
 @app.route("/welcome", methods=['GET', 'POST'])
 def welcome():
     form = welcomeForm()
@@ -20,25 +22,26 @@ def welcome():
     return render_template('welcome.html', title='Welcome', form=form)
 
 
-@app.route("/newswebsite1")
-def newsWebsite1():
-    return render_template('newswebsite1_viral.html')
-
-
-# route to our demographic information form
+# route to demographic information form
 @app.route("/demographics", methods=['GET', 'POST'])
 def demographics():
     form = demographicsForm()
     if form.validate_on_submit():
         # randomize websiteList
         randomWebsiteList = random.sample(websiteList, len(websiteList))
+        # save data to db
+        participant = demographicData(gender=form.gender.data, age=form.age.data, nationality=form.nationality.data)
+        db.session.add(participant)
+        db.session.commit()
         return redirect(randomWebsiteList[0])
     return render_template('demographics.html', title='Demographic Information', form=form)
 
 
+# route to cover-story questionnaire
 @app.route("/newswebsitedesign", methods=['GET', 'POST'])
 def websiteDesign():
     form = websiteDesignForm()
+    return render_template('websiteDesign.html', title='News website design', form=form)
 
 
 # route to perceived control and deliberation form
