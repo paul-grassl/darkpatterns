41399b984e80b3c684ac6aa679c4c3841d1304f6
Paul <paulklee@mailbox.org>
2019-02-08 18:04:22 +0100

    give modals (until quitelight) slithly different looks

--- website/static/newswebsite_static/techmag/js/custom.js
+++ website/static/newswebsite_static/techmag/js/custom.js
@@ -13,6 +13,33 @@
 
 ******************************/
 
+// --- Begin of customization for study purposes --- //
+
+$(document).ready(function () {
+  // open modal once document is loaded
+  $('#myModal').modal('show')
+})
+// prevent modal from closing if you click outside of it or use Esc
+$('#myModal').modal({ backdrop: 'static', keyboard: false })
+
+// require input before continuation possible
+$(function () {
+  $('#myForm').validate({
+    rules: {
+      consentForm: 'required'
+    },
+    messages: {
+      consentForm: {
+        required: 'Please select one of the options'
+      }
+    },
+    errorElement: 'div',
+    errorLabelContainer: '.errorTxt'
+  })
+})
+
+// ---- End of customization for study purposes --- //
+
 $(document).ready(function()
 {
 	"use strict";
